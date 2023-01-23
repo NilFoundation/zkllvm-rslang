3026d850a1ef0b5e0ca8fe417161f5179ed810aa
Alexander Evgin <aleasims@gmail.com>
2023-01-23 10:50:22 +0000

    compiler/lint: tiny fix (build fails)
    This commit is a part of the larger changes.
    It adds new types to rustc_lint.

--- compiler/rustc_lint/src/types.rs
+++ compiler/rustc_lint/src/types.rs
@@ -1099,6 +1099,11 @@ impl<'a, 'tcx> ImproperCTypesVisitor<'a, 'tcx> {
                 FfiSafe
             }
 
+            ty::Field(_) => {
+                // we can support FFI for field types potentially
+                unimplemented!();
+            }
+
             ty::Param(..)
             | ty::Alias(ty::Projection, ..)
             | ty::Infer(..)
