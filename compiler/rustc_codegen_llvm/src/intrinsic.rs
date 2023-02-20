af7dbc1f7589a68c386c844d88960c9e202f54b3
Alexander Evgin <aleasims@gmail.com>
2023-02-20 19:04:48 +0000

    rustc_codegen_llvm: some placeholders for new field ABI

--- compiler/rustc_codegen_llvm/src/intrinsic.rs
+++ compiler/rustc_codegen_llvm/src/intrinsic.rs
@@ -154,9 +154,6 @@ impl<'ll, 'tcx> IntrinsicCallMethods<'tcx> for Builder<'_, 'll, 'tcx> {
                             }
                             // `va_arg` should never be used with the return type f32.
                             Primitive::F32 => bug!("the va_arg intrinsic does not work with `f32`"),
-                            Primitive::Field(..) => {
-                                todo!()
-                            },
                         }
                     }
                     _ => bug!("the va_arg intrinsic does not work with non-scalar types"),
@@ -302,7 +299,7 @@ impl<'ll, 'tcx> IntrinsicCallMethods<'tcx> for Builder<'_, 'll, 'tcx> {
                 let tp_ty = substs.type_at(0);
                 let layout = self.layout_of(tp_ty).layout;
                 let use_integer_compare = match layout.abi() {
-                    Scalar(_) | ScalarPair(_, _) => true,
+                    Scalar(_) | ScalarPair(_, _) | Field(_) => true,
                     Uninhabited | Vector { .. } => false,
                     Aggregate { .. } => {
                         // For rusty ABIs, small aggregates are actually passed
