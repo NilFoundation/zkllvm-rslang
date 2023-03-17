5526fca613b98363dacfef50051a4423e035fd6d
Alexander Evgin <aleasims@gmail.com>
2023-03-17 13:44:02 +0000

    rustc_target: add llvm-link as new linker for assigner target

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -149,6 +149,8 @@ pub enum LinkerFlavor {
     Bpf,
     /// Linker tool for Nvidia PTX.
     Ptx,
+    /// Linker tool for LLVM IR (for "assigner" target).
+    LlvmLink,
 }
 
 /// Linker flavors available externally through command line (`-Clinker-flavor`)
@@ -164,6 +166,7 @@ pub enum LinkerFlavorCli {
     Em,
     BpfLinker,
     PtxLinker,
+    LlvmIrLinker,
 }
 
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
@@ -231,6 +234,7 @@ impl LinkerFlavor {
             LinkerFlavorCli::Em => LinkerFlavor::EmCc,
             LinkerFlavorCli::BpfLinker => LinkerFlavor::Bpf,
             LinkerFlavorCli::PtxLinker => LinkerFlavor::Ptx,
+            LinkerFlavorCli::LlvmIrLinker => LinkerFlavor::LlvmLink,
         }
     }
 
@@ -251,6 +255,7 @@ impl LinkerFlavor {
             LinkerFlavor::EmCc => LinkerFlavorCli::Em,
             LinkerFlavor::Bpf => LinkerFlavorCli::BpfLinker,
             LinkerFlavor::Ptx => LinkerFlavorCli::PtxLinker,
+            LinkerFlavor::LlvmLink => LinkerFlavorCli::LlvmIrLinker,
         }
     }
 
@@ -260,6 +265,7 @@ impl LinkerFlavor {
             | LinkerFlavor::Unix(..)
             | LinkerFlavor::EmCc
             | LinkerFlavor::Bpf
+            | LinkerFlavor::LlvmLink
             | LinkerFlavor::Ptx => LldFlavor::Ld,
             LinkerFlavor::Darwin(..) => LldFlavor::Ld64,
             LinkerFlavor::WasmLld(..) => LldFlavor::Wasm,
@@ -306,6 +312,7 @@ linker_flavor_cli_impls! {
     (LinkerFlavorCli::Em) "em"
     (LinkerFlavorCli::BpfLinker) "bpf-linker"
     (LinkerFlavorCli::PtxLinker) "ptx-linker"
+    (LinkerFlavorCli::LlvmIrLinker) "llvm-link"
 }
 
 impl ToJson for LinkerFlavorCli {
@@ -1694,6 +1701,7 @@ fn add_link_args_iter(
         | LinkerFlavor::Unix(..)
         | LinkerFlavor::EmCc
         | LinkerFlavor::Bpf
+        | LinkerFlavor::LlvmLink
         | LinkerFlavor::Ptx => {}
     }
 }
