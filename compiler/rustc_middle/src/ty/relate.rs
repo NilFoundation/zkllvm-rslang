037a401045ba702bb13722a4628b24e27f4ed9cb
Alexander Evgin <aleasims@gmail.com>
2023-09-01 13:19:05 +0000

    rustc_middle: fix type relation for curve types

--- compiler/rustc_middle/src/ty/relate.rs
+++ compiler/rustc_middle/src/ty/relate.rs
@@ -422,6 +422,7 @@ pub fn super_relate_tys<'tcx, R: TypeRelation<'tcx>>(
         | (&ty::Int(_), _)
         | (&ty::Uint(_), _)
         | (&ty::Field(_), _)
+        | (&ty::Curve(_), _)
         | (&ty::Float(_), _)
         | (&ty::Str, _)
             if a == b =>
