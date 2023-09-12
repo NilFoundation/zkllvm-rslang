deca808e919d96949f54cdbd6980ed80815e0a5b
Alexander Evgin <aleasims@gmail.com>
2023-09-12 19:59:13 +0000

    library/core: implement `from_coordinates` for curves

--- library/core/src/num/curves.rs
+++ library/core/src/num/curves.rs
@@ -4,6 +4,29 @@
 
 use crate::intrinsics;
 
+macro_rules! impl_from_coordinates {
+    ($($t:ty, $f:ty)*) => {
+        $(
+            #[stable(feature = "rust1", since = "1.0.0")]
+            impl $t {
+                /// Create curve element from its base field coordinates.
+                #[inline(always)]
+                #[stable(feature = "rust1", since = "1.0.0")]
+                pub unsafe fn from_coordinates(x: $f, y: $f) -> Self {
+                    unsafe { intrinsics::curve_init::<$f, $t>(x, y) }
+                }
+            }
+        )*
+    }
+}
+
+impl_from_coordinates! {
+    __zkllvm_curve_bls12381, __zkllvm_field_bls12381_base
+    __zkllvm_curve_curve25519, __zkllvm_field_curve25519_base
+    __zkllvm_curve_pallas, __zkllvm_field_pallas_base
+    __zkllvm_curve_vesta, __zkllvm_field_pallas_scalar
+}
+
 macro_rules! impl_zero_one {
     ($($t:ty, $f:ty, $zero_x:expr, $zero_y:expr, $one_x:expr, $one_y:expr)*) => {
         $(
@@ -13,14 +36,14 @@ macro_rules! impl_zero_one {
                 #[inline(always)]
                 #[stable(feature = "rust1", since = "1.0.0")]
                 pub fn zero() -> Self {
-                    unsafe { intrinsics::curve_init::<$f, $t>($zero_x, $zero_y) }
+                    unsafe { Self::from_coordinates($zero_x, $zero_y) }
                 }
 
                 /// Returns curve generator (`one`).
                 #[inline(always)]
                 #[stable(feature = "rust1", since = "1.0.0")]
                 pub fn one() -> Self {
-                    unsafe { intrinsics::curve_init::<$f, $t>($one_x, $one_y) }
+                    unsafe { Self::from_coordinates($one_x, $one_y) }
                 }
             }
         )*
