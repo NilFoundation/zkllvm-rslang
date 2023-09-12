07cda0ddec953a31cb74372a888afc9cd6e10bcf
Alexander Evgin <aleasims@gmail.com>
2023-09-12 19:13:29 +0000

    library/core: dummy impl of `zero` and `one` functions for curves
    Actual initialization values are still needed to be applied here.
    For now leaving these dummy values.

--- library/core/src/num/curves.rs
+++ library/core/src/num/curves.rs
@@ -1,10 +1,11 @@
-//! Elliptic curve elements.
+//! Elliptic curve functions.
 
 #![stable(feature = "rust1", since = "1.0.0")]
 
-#[cfg(not(bootstrap))]
-macro_rules! impl_curve_elem {
-    ($($t:ty)*) => {
+use crate::intrinsics;
+
+macro_rules! impl_zero_one {
+    ($($t:ty, $f:ty, $zero_x:expr, $zero_y:expr, $one_x:expr, $one_y:expr)*) => {
         $(
             #[stable(feature = "rust1", since = "1.0.0")]
             impl $t {
@@ -12,24 +13,25 @@ macro_rules! impl_curve_elem {
                 #[inline(always)]
                 #[stable(feature = "rust1", since = "1.0.0")]
                 pub fn zero() -> Self {
-                    todo!();
+                    unsafe { intrinsics::curve_init::<$f, $t>($zero_x, $zero_y) }
                 }
 
                 /// Returns curve generator (`one`).
                 #[inline(always)]
                 #[stable(feature = "rust1", since = "1.0.0")]
                 pub fn one() -> Self {
-                    todo!();
+                    unsafe { intrinsics::curve_init::<$f, $t>($one_x, $one_y) }
                 }
             }
         )*
     }
 }
 
-#[cfg(not(bootstrap))]
-impl_curve_elem! {
-    __zkllvm_curve_bls12381
-    __zkllvm_curve_curve25519
-    __zkllvm_curve_pallas
-    __zkllvm_curve_vesta
+// FIXME: (aleasims) replace these dummy initialization values with real one
+// These are here only because I cannot place todo!() here.
+impl_zero_one! {
+    __zkllvm_curve_bls12381, __zkllvm_field_bls12381_base, 0g, 0g, 1g, 1g
+    __zkllvm_curve_curve25519, __zkllvm_field_curve25519_base, 0g, 0g, 1g, 1g
+    __zkllvm_curve_pallas, __zkllvm_field_pallas_base, 0g, 0g, 1g, 1g
+    __zkllvm_curve_vesta, __zkllvm_field_pallas_scalar, 0g, 0g, 1g, 1g
 }
